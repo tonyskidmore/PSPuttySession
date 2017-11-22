1a12502690fef3e9cf11d9906fdeb24ccde5ff09
Tony Skidmore <anthony.skidmore@accenture.com>
2017-11-22 21:46:34 +0000

    updating retries

--- PSPuttySession.ps1
+++ PSPuttySession.ps1
@@ -140,13 +140,113 @@ function Get-Putty
     $outputFile = "$tmpDir\putty.exe"
 
 
-    #try {
-        Invoke-WebRequest -Uri $Url -OutFile $outputFile -ErrorAction Stop
-    #}
-    #catch {
-    #    Write-Error -Message "Failed to download putty"
-    #}
+    try {
+        if(-not (Test-Path -Path $outputFile)) {
+            Invoke-WebRequest -Uri $Url -OutFile $outputFile -ErrorAction Stop
+        }
+    }
+    catch {
+        Write-Error -Message "Failed to download putty"
+    }
 
+}
+
+function Set-HostKey
+{
+
+    $HostKey = '0x10001,0xb5c2a1b057775cc1336dcd8edc08a60d3648970c39b489f904a02352abd617332815d6f94cc9017209c26345a133379d4c9a8552d34c6d4a1d6539a8d5d015068868e19c28e215243de5587383b8dca8a270942dcb781ab05542e6f7e7d80f63fbf3a0390aa8dba6dac230c74682d475dfb97a9bea085e6f406cce64
+eeb0baab350dd252563e8b609baf6c465a8f9d3c74af745a3567ac835c19b548f0d409910029bda89e16ade19869bd043271daf1532160c5088b8bb3b6c4eb77f766bd42c2e2299794edad1b17cf8eee9d8e152321ff75f99c1715d061361cf4a919eddfe38416e9aa577afde28d9d9eb42aa0d117e6ef9fa8e9665932c643f3ba
+ac0971'
+
+    $regName = 'rsa2@22:jumphost.cloud-msp.net'
+
+    try {
+        $newProperty = New-ItemProperty -Path "HKCU:\software\SimonTatham\PuTTY\SshHostKeys" -Name $regName -Value $HostKey -PropertyType String -Force -ErrorAction Stop | Out-Null
+    }
+    catch {
+        Write-Error -Message "Failed to set hostkey"        
+    }
+    
+}
+
+function Get-Key
+{
+    Param(
+
+        $Url = "http://jumphost.cloud-msp.net/$UserName.ppk"
+
+    )
+
+    $outputFile = "$tmpDir\$UserName.ppk"
+    $retries = 20
+    $sleepSeconds = 10
+
+    for($i = 1 ; $i -lt $retries ; $i++) {
+        Write-Progress -Activity "Attempting to acquire the key" -PercentComplete ($i/$retries*100)
+
+        if(Test-Path -Path $outputFile) {
+            $keyFound = $true
+            Write-Progress -Activity "Attempting to acquire the key" -Completed
+            break
+        }
+        else {
+            try {
+                Invoke-WebRequest -Uri $Url -OutFile $outputFile -ErrorAction SilentlyContinue
+            }
+            catch {
+
+            }
+            if(Test-Path -Path $outputFile) {
+                $keyFound = $true
+                Write-Progress -Activity "Attempting to acquire the key" -Completed
+                break
+            }
+            else {
+                Write-Progress -Activity "Attempting to acquire the key" -PercentComplete ($i/$retries*100) -Status "$i of $retries attempts" -CurrentOperation "Sleeping $sleepSeconds seconds"
+                Start-Sleep -Seconds $sleepSeconds
+            }
+        }
+    }
+
+    if(-not $keyFound) { 
+        Write-Progress -Activity "Failed to acquire the key" -Completed
+        Write-Error "Unable to obtain the key"
+        return $false
+    } else {
+        Write-Output "Key obtained"
+        return $true
+    }
+
+
+}
+
+function Invoke-PuttySession
+{
+    Param(
+
+        [string]
+        $Hostname = 'jumphost.cloud-msp.net',
+
+        [string]
+        $User = $UserName,
+
+        [string]
+        $PrivateKey = (Join-Path -Path $tmpDir -ChildPath "$UserName.ppk"),
+
+        [string]
+        $HostKey = '3d:de:ea:73:7b:d4:43:8f:d1:f5:41:c4:ca:b0:39:a7'
+    )
+
+    try {
+        $puttyPath = Join-Path -Path $tmpDir -ChildPath 'putty.exe'
+        $argumentList = "$User@$Hostname -i $PrivateKey -hostkey $HostKey"
+        Write-Output $puttyPath
+        Write-Output $argumentList
+        $result = Start-Process -FilePath $puttyPath -ArgumentList $argumentList -NoNewWindow -ErrorAction Stop
+    }
+    catch {
+        Write-Error -Message "Failed to run putty session"
+    }
 }
 
 function New-ProjectSpace
@@ -175,6 +275,9 @@ Wait-Random
 Start-AwxTemplate
 New-ProjectSpace
 Get-Putty
+Set-HostKey
+if(Get-Key) { 
+    Write-Output "Your username is: $UserName"    
+    Invoke-PuttySession 
+}
 
-
-Write-Output "Your username is: $UserName"
